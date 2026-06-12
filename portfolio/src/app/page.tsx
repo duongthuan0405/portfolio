fd3a79d9239980d98205320cad0909f6cdf16ead
duongthuan0405 <duongthuan0405@gmail.com>
2026-06-12 10:23:43 +0700

    ADD: .yml file - CI when PR to main

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+export const dynamic = "force-dynamic";
 
-export default function Home() {
+export default async function Home() {
+  const data: [] = await fetch("http://localhost:8000/").then((res) =>
+    res.json(),
+  );
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Duong Quoc Thuan
+      {data.map((item: any) => (
+        <div key={item.id}>
+          <h2>
+            {item.id} {item.name} {item.gpa}
+          </h2>
+        </div>
+      ))}
     </div>
   );
 }
