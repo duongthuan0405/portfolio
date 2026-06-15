aae9b3d78893e28aa53ed8ed7d47c7c0fdd49e10
duongthuan0405 <duongthuan0405@gmail.com>
2026-06-15 13:04:46 +0700

    FIX: Light theme color

--- duongthuan-portfolio/src/components/CardInfo/EducationInfo.tsx
+++ duongthuan-portfolio/src/components/CardInfo/EducationInfo.tsx
@@ -47,7 +47,7 @@ const EducationInfo = function ({ educationInfoList }: EducationInfoProps) {
                 key={`education_info_${i}`}
                 className="rounded-2xl flex flex-col lg:flex-row items-stretch gap-0.5"
               >
-                <div className="flex flex-col p-3 gap-1 bg-linear-to-br from-foreground/5 to-foreground/15 lg:rounded-l-2xl max-lg:rounded-t-2xl flex-3">
+                <div className="flex flex-col p-6 gap-1 bg-linear-to-br from-foreground/5 to-foreground/15 lg:rounded-l-2xl max-lg:rounded-t-2xl flex-3">
                   <div className="flex gap-1 flex-wrap">
                     <h1 className="font-bold">{"Education: "}</h1>
                     <p>{educationInfo.university}</p>
@@ -82,7 +82,7 @@ const EducationInfo = function ({ educationInfoList }: EducationInfoProps) {
                   <img
                     src={educationInfo.image}
                     alt={educationInfo.university}
-                    className="object-contain absolute w-full h-full"
+                    className="object-cover absolute w-full h-full"
                   ></img>
                 </div>
               </CarouselItem>
