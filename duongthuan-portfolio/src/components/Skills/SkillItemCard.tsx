3ee2a4a63c9ee01829bd408df4399500377016b0
duongthuan0405 <duongthuan0405@gmail.com>
2026-06-17 22:56:40 +0700

    ADD: Add all skills

---
+++ duongthuan-portfolio/src/components/Skills/SkillItemCard.tsx
@@ -0,0 +1,20 @@
+import { SkillItem } from "@/dataProvider/skills";
+
+type SkillItemCardProps = {
+  skillItem: SkillItem;
+};
+const SkillItemCard = function ({ skillItem }: SkillItemCardProps) {
+  return (
+    <div className="flex flex-col gap-1 items-center text-xl">
+      <img
+        src={skillItem.icon}
+        alt={skillItem.name}
+        className="bg-card-skill/90 rounded-2xl p-5 aspect-square w-full"
+      />
+      <p className="font-semibold">{skillItem.name}</p>
+      <p>{skillItem.level}</p>
+    </div>
+  );
+};
+
+export default SkillItemCard;
--- duongthuan-portfolio/src/dataProvider/skills.ts
+++ duongthuan-portfolio/src/dataProvider/skills.ts
@@ -1,6 +1,7 @@
 export type SkillItem = {
   name: string;
-  icon?: string; // standard class/identifier, or path to SVG/icon
+  icon?: string;
+  level: "Beginner" | "Basic" | "Intermediate" | "Advanced" | "Expert";
 };
 
 export type SkillCategory = {
@@ -11,13 +12,29 @@ export type SkillCategory = {
 
 const skillsData: SkillCategory[] = [
   {
-    id: "programing_language",
-    title: "Programing Languages",
+    id: "programming_language",
+    title: "Programming Languages",
     skills: [
-      { name: "C++", icon: "cpp" },
-      { name: "C#", icon: "cs" },
-      { name: "JavaScript", icon: "js" },
-      { name: "TypeScript", icon: "ts" },
+      {
+        name: "C++",
+        icon: "/skills/programming_languages/cpp.svg",
+        level: "Intermediate",
+      },
+      {
+        name: "C#",
+        icon: "/skills/programming_languages/csharp.svg",
+        level: "Intermediate",
+      },
+      {
+        name: "JavaScript",
+        icon: "/skills/programming_languages/javascript.svg",
+        level: "Intermediate",
+      },
+      {
+        name: "TypeScript",
+        icon: "/skills/programming_languages/typescript.svg",
+        level: "Basic",
+      },
     ],
   },
 
@@ -25,41 +42,96 @@ const skillsData: SkillCategory[] = [
     id: "frontend",
     title: "Frontend Development",
     skills: [
-      { name: "HTML5", icon: "html5" },
-      { name: "CSS3", icon: "css3" },
-      { name: "JavaScript", icon: "javascript" },
-      { name: "TypeScript", icon: "typescript" },
-      { name: "React", icon: "react" },
-      { name: "Next.js", icon: "nextjs" },
-      { name: "Tailwind CSS", icon: "tailwindcss" },
+      {
+        name: "HTML5",
+        icon: "skills/frontend_developments/html5.svg",
+        level: "Basic",
+      },
+      {
+        name: "CSS3",
+        icon: "skills/frontend_developments/css.svg",
+        level: "Basic",
+      },
+      {
+        name: "React.js",
+        icon: "skills/frontend_developments/react.svg",
+        level: "Intermediate",
+      },
+      {
+        name: "Next.js",
+        icon: "skills/frontend_developments/nextjs.svg",
+        level: "Beginner",
+      },
+      {
+        name: "Tailwind CSS",
+        icon: "skills/frontend_developments/tailwindcss.svg",
+        level: "Basic",
+      },
     ],
   },
   {
     id: "backend",
     title: "Backend Development",
     skills: [
-      { name: "Node.js", icon: "nodejs" },
-      { name: "Express.js", icon: "express" },
-      { name: "NestJS", icon: "nestjs" },
-      { name: "RESTful API", icon: "api" },
+      {
+        name: "ASP .NET",
+        icon: "/skills/backend_developments/dotnet.svg",
+        level: "Intermediate",
+      },
+      {
+        name: "Node.js",
+        icon: "/skills/backend_developments/nodejs.svg",
+        level: "Beginner",
+      },
+      {
+        name: "Express.js",
+        icon: "/skills/backend_developments/expressjs.svg",
+        level: "Basic",
+      },
+      {
+        name: "RESTful API",
+        icon: "/skills/backend_developments/rest-api.svg",
+        level: "Intermediate",
+      },
     ],
   },
   {
     id: "database",
     title: "Database",
     skills: [
-      { name: "PostgreSQL", icon: "postgresql" },
-      { name: "SQL Server", icon: "sqlserver" },
+      {
+        name: "PostgreSQL",
+        icon: "/skills/databases/postgresql.svg",
+        level: "Intermediate",
+      },
+      {
+        name: "SQL Server",
+        icon: "/skills/databases/sql-server.svg",
+        level: "Intermediate",
+      },
     ],
   },
   {
     id: "devops",
     title: "DevOps",
     skills: [
-      { name: "Docker", icon: "docker" },
-      { name: "Kubernetes", icon: "kubernetes" },
+      {
+        name: "Docker",
+        icon: "/skills/devops/docker.svg",
+        level: "Basic",
+      },
+      {
+        name: "Kubernetes",
+        icon: "/skills/devops/k8s.svg",
+        level: "Basic",
+      },
+      {
+        name: "GitHub Action",
+        icon: "/skills/devops/github-action.svg",
+        level: "Basic",
+      },
     ],
-  }
+  },
 ];
 
 export default skillsData;
