6319eded8ce2723bc13831cb380897b43a01bd03
duongthuan0405 <duongthuan0405@gmail.com>
2026-06-21 22:01:27 +0700

    ADD: Real exp

--- duongthuan-portfolio/src/app/(index)/(sections)/SkillsSection.tsx
+++ duongthuan-portfolio/src/app/(index)/(sections)/SkillsSection.tsx
@@ -5,7 +5,7 @@ const SkillsSection = function () {
   return (
     <section
       id="skills"
-      className="flex flex-col items-center gap-10 w-full pl-5 md:pl-10 lg:pl-15 xl:pl-20 md:pr-5 lg:pr-7.5 xl:pr-10"
+      className="flex flex-col items-center gap-10 w-full lg:px-10 xl:px-20"
     >
       <div className="text-4xl font-bold ">SKILLS</div>
       <div className="flex flex-col gap-8 w-full border-l border-foreground/20 pl-8 md:pl-10">
